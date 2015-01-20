7ce90bde97ec1f787513495faecfc16bc287bd94
xarph <git@xarph.com>
2015-01-19 19:49:02 -0800

    remove pdf support (didn't work)
    now operates from directory tree

--- natgeoconv.py
+++ natgeoconv.py
@@ -14,13 +14,6 @@ def decodepage(inputpath,outputpath):
         jpeg[byte] ^= 0xEF
     open(outputpath,'wb').write(jpeg)
 
-def makepdf(jpegdir,outpath):
-    jpeglist = []
-    for filename in sorted(os.listdir(jpegdir)):
-        if os.path.splitext(filename)[1] == '.jpg':
-            jpeglist.append(os.path.join(jpegdir,filename))
-    subprocess.check_call(shlex.split("convert -compress jpeg -format pdf {jpegfiles} {outpath}".format(jpegfiles=' '.join(jpeglist), outpath=outpath)))
-
 def makecbz(jpegdir,outpath):
     cbzfile = zipfile.ZipFile(outpath,'w')
     for filename in sorted(os.listdir(jpegdir)):
@@ -28,22 +21,23 @@ def makecbz(jpegdir,outpath):
             cbzfile.write(os.path.join(jpegdir,filename))
     cbzfile.close()
     
+def processdir(dirname, names):
+    """Returns path to decoded jpegs or None if nothing decoded"""
+    decodedfiles = False
+    tempdir = tempfile.mkdtemp()
+    for filename in names:
+        if os.path.splitext(filename)[1] == '.cng':
+            decodepage(os.path.join(dirname,filename),os.path.join(tempdir,os.path.splitext(filename)[0]+".jpg"))
+            decodedfiles = True
+    return tempdir if decodedfiles else None
+        
 def main():
     if len(sys.argv) != 3:
-        sys.exit('Usage: %s SOURCEDIR OUTFILE' % sys.argv[0])
-    tempdir = tempfile.mkdtemp()
-    print tempdir
+        sys.exit('Usage: %s SOURCEDIR OUTDIR' % sys.argv[0])
     sourcedir = sys.argv[1]
-    for i in os.listdir(sourcedir):
-        if os.path.splitext(i)[1] == '.cng':
-            print os.path.join(sourcedir,i)
-            decodepage(os.path.join(sourcedir,i),os.path.join(tempdir,os.path.splitext(i)[0]+'.jpg'))
-    if os.path.splitext(sys.argv[2])[1] == '.pdf':
-        makepdf(tempdir,sys.argv[2])
-    elif os.path.splitext(sys.argv[2])[1] == '.cbz':
-        makecbz(tempdir,sys.argv[2])
-    shutil.rmtree(tempdir)
-    
+    for (dirpath, dirnames, filenames) in os.walk(sourcedir):
+        jpegdir = processdir(dirpath,filenames)
+        if jpegdir != None: makecbz(jpegdir,os.path.join(sys.argv[2],os.path.split(dirpath)[-1]+'.cbz'))
     
 if __name__ == "__main__":
     main()
